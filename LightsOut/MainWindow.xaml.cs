34b8e13ff1033f3dce5d14375679bf66f24d3309
AizbergK <106670834+AizbergK@users.noreply.github.com>
2025-03-28 13:06:24 +0200

    Add project files.

---
+++ LightsOut/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LightsOut;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public static MainWindow? Instance { get; private set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Instance = this;
+        Instance.ChangeWindow(new Uri("./Sign_in.xaml", UriKind.Relative));
+        Instance.ResizeMode = ResizeMode.NoResize;
+    }
+    public void ChangeWindow(System.Uri WindowName)
+    {
+        AppWindow.NavigationService.Navigate(WindowName);
+    }
+}
---
+++ LightsOut/Menu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LightsOut
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void StartGameClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./Game_Page.xaml", UriKind.Relative));
+        }
+        private void StatisticsClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./Statistics.xaml", UriKind.Relative));
+        }
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./Settings.xaml", UriKind.Relative));
+        }
+        private void AboutClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./About.xaml", UriKind.Relative));
+        }
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./Sign_in.xaml", UriKind.Relative));
+        }
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeWindow(new Uri("./Sign_in.xaml", UriKind.Relative));
+        }
+    }
+}
+
